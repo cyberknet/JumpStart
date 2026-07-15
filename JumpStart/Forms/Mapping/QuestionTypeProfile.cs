8c5ed2354fce590334d2b05fc56bcf9a1d23b1f9
Scott Blomfield <github@cyberknet.net>
2026-07-15 10:06:06 -0500

    Fix QuestionTypeProfile using the wrong update DTO (UpdateQuestionDto instead of UpdateQuestionTypeDto)

--- JumpStart/Forms/Mapping/QuestionTypeProfile.cs
+++ JumpStart/Forms/Mapping/QuestionTypeProfile.cs
@@ -11,12 +11,13 @@ namespace JumpStart.Forms.Mapping;
 /// AutoMapper profile for QuestionType entity to DTO mappings.
 /// </summary>
 /// <remarks>
-/// Configures mappings between <see cref="QuestionType"/>, <see cref="QuestionTypeDto"/>, <see cref="QuestionTypeDto"/>.
+/// Configures mappings between <see cref="QuestionType"/>, <see cref="QuestionTypeDto"/>,
+/// <see cref="CreateQuestionTypeDto"/>, and <see cref="UpdateQuestionTypeDto"/>.
 /// </remarks>
-public class QuestionTypeProfile : EntityMappingProfile<QuestionType, QuestionTypeDto, CreateQuestionTypeDto, UpdateQuestionDto>
+public class QuestionTypeProfile : EntityMappingProfile<QuestionType, QuestionTypeDto, CreateQuestionTypeDto, UpdateQuestionTypeDto>
 {
     /// <inheritdoc/>
-    protected override void ConfigureAdditionalMappings(IMappingExpression<QuestionType, QuestionTypeDto> entityMap, IMappingExpression<CreateQuestionTypeDto, QuestionType> createMap, IMappingExpression<UpdateQuestionDto, QuestionType> updateMap)
+    protected override void ConfigureAdditionalMappings(IMappingExpression<QuestionType, QuestionTypeDto> entityMap, IMappingExpression<CreateQuestionTypeDto, QuestionType> createMap, IMappingExpression<UpdateQuestionTypeDto, QuestionType> updateMap)
     {
         createMap
             .ForMember(dest => dest.Questions, opt => opt.Ignore());
